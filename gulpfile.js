0f275adf1c729d80fcfe033827c5692d9e71234a
Pavel <wol1414@gmail.com>
2022-08-10 10:57:51 +0300

    увше

--- gulpfile.js
+++ gulpfile.js
@@ -229,10 +229,12 @@ function startwatch() {
     watch(sourse + '/sass/*.svg', { usePolling: true }, svgCopy);
 
     watch([sourse + '/js/common.js'], { usePolling: true }, common);
-    watch(sourse + '/img', { usePolling: true }, img);
+    // watch(sourse + '/img', { usePolling: true }, img);
 }
 
 export let imgAll = series(cleanimg, img)
 export let libs = series(cleanlibs, copyLibs)
 export let sprite = series(svg, svgCopy)
-export default series(common, libs, styles, imgAll, sprite, pugFiles, parallel(browsersync, startwatch))
+export default series(common, libs, styles, 
+    // imgAll,
+     sprite, pugFiles, parallel(browsersync, startwatch))
